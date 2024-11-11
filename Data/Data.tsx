d6bffb7d83c3f1e146706115a8f877609b3de09b
JuanDiegoPaezDelgado <usr3859@salesianos-lacuesta.net>
2024-11-11 21:40:52 +0000

    feat: Creacion de los datos con los que trabajaremos

---
+++ Data/Data.tsx
@@ -0,0 +1,22 @@
+export const cardsData = [
+  { text: "Salir a pasear" },
+  { text: "Leer manga" },
+  { text: "Leer novelas" },
+  { text: "Lol" },
+  { text: "Dormir" },
+  { text: "El monte con lluvia" },
+  { text: "Viajar" },
+  { text: "Música en ingles" },
+  { text: "Anime" },
+  { text: "Ducharme" },
+  { text: "Videojuegos" },
+  { text: "Pensar en pepe" },
+];
+
+export const dontLikeCardsData = [
+  { text: "Programacion multimedia" },
+  { text: "Programacion de servicios" },
+  { text: "El calor" },
+  { text: "El verano" },
+  { text: "Sudar" },
+];
---
+++ Data/DescriptionData.tsx
@@ -0,0 +1,6 @@
+export const descriptionData = {
+  image: require("../assets/SofyanAmrabat.jpg"),
+  title: "Descripción sobre mí!",
+  description:
+    "Soy un estudiante de 2 dam en salesianos la cuesta, estoy aprendiendo a programar para poder trabajar y no morirme de hambre",
+};
