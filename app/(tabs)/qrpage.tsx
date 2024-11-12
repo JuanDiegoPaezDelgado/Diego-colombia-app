3347f9203a2b3f1cce99e3cef09b2a0bf8059831
JuanDiegoPaezDelgado <usr3859@salesianos-lacuesta.net>
2024-11-12 12:00:24 +0000

    refactor(qrpage): refactorizacion de qrpage para usar componente myQR

--- app/(tabs)/qrpage.tsx
+++ app/(tabs)/qrpage.tsx
@@ -1,15 +1,9 @@
 import { View, Text } from "react-native";
 import React from "react";
 import QRCode from "react-native-qrcode-svg";
-
-const MyQr = () => {
-  return (
-    <View style={{ justifyContent: "center" }}>
-      <View style={{ flex: 1, alignItems: "center" }}>
-        <QRCode value="https://github.com/JuanDiegoPaezDelgado" />
-      </View>
-    </View>
-  );
+import MyQr from "../../componentes/MyQr";
+const Qr = () => {
+  return <MyQr />;
 };
 
-export default MyQr;
+export default Qr;
