fe42c9c3d38c1987bce2c04647836926eaccc2b9
JuanDiegoPaezDelgado <usr3859@salesianos-lacuesta.net>
2024-11-12 12:28:21 +0000

    refactor(qrpage): agregacion del header

--- app/(tabs)/qrpage.tsx
+++ app/(tabs)/qrpage.tsx
@@ -1,9 +1,13 @@
-import { View, Text } from "react-native";
 import React from "react";
-import QRCode from "react-native-qrcode-svg";
 import MyQr from "../../componentes/MyQr";
+import Header from "../../componentes/Header";
 const Qr = () => {
-  return <MyQr />;
+  return (
+    <>
+      <Header />
+      <MyQr />
+    </>
+  );
 };
 
 export default Qr;
