67b3065ec2a61125c0eb8e67a37a61552c9fffcd
JuanDiegoPaezDelgado <usr3859@salesianos-lacuesta.net>
2025-01-26 17:44:56 +0000

    feat: creacion de register con sus validaciones y api

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import Login from "./userSignMethods/Login";
 import Register from "./userSignMethods/Register";
 
 const StartPage = () => {
-  return Register; //<Redirect href="componentes/userSignMethods/Login" />;<Login />
+  return <Register />; //<Redirect href="componentes/userSignMethods/Login" />;<Login />
 };
 
 export default StartPage;
--- app/userSignMethods/Register.tsx
+++ app/userSignMethods/Register.tsx
@@ -1,33 +1,124 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
   TextInput,
-  Button,
   StyleSheet,
-  TouchableOpacity,
   Pressable,
+  Alert,
 } from "react-native";
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
 
 const Register = () => {
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const validateEmail = (email: string) => {
+    const regex = /.+@.+\..+/;
+    return regex.test(email);
+  };
+
+  const isPasswordSafe = (password: string) => {
+    return (
+      password.length >= 8 &&
+      /[0-9]/.test(password) &&
+      /[a-zA-Z]/.test(password)
+    );
+  };
+
+  const handleRegister = async () => {
+    if (!fullname) {
+      Alert.alert("Nombre vacio", "Introduzca un nombre completo");
+      return;
+    }
+    if (!email) {
+      Alert.alert("Email vacio", "Introduzca un email.");
+      return;
+    }
+    if (!validateEmail(email)) {
+      Alert.alert("Email invalido", "Introduce un email valido");
+      return;
+    }
+    if (!password) {
+      Alert.alert("Contraseña vacia", "Introduzca una contraseña");
+      return;
+    }
+    if (!isPasswordSafe(password)) {
+      Alert.alert(
+        "Contraseña invalida",
+        "La contraseña debe tener al menos 8 caracteres y ademas debe tener numeros y letras"
+      );
+      return;
+    }
+
+    try {
+      const apiUrl = "http://192.168.1.135:5000/auth/register";
+      const response = await fetch(apiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ fullname, email, pswd: password }),
+      });
+
+      if (response.status === 201) {
+        Alert.alert("Registrado", "El registro ha sido completado!");
+        Alert.alert("Registro exitoso");
+        router.navigate("/userSignMethods/Login");
+      } else if (response.status === 400) {
+        Alert.alert(
+          "Error en el servidor",
+          "La solicitud no pudo ser procesada"
+        );
+      } else if (response.status === 409) {
+        Alert.alert(
+          "Usuario ya existe",
+          "Ya existe un usuario registrado con ese email."
+        );
+      } else {
+        const errorData = await response.json();
+        Alert.alert(
+          "Error en el registro",
+          errorData.message || "Algo salió mal durante el registro."
+        );
+      }
+    } catch (error) {
+      console.error("Error de red o al llamar a la API:", error);
+      Alert.alert(
+        "Error",
+        "Error al conectar con el servidor. Inténtalo de nuevo."
+      );
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.secondContainer}>
         <Text style={styles.title}>REGISTRAR</Text>
-        <TextInput style={styles.input} placeholder="Nombre completo" />
+        <TextInput
+          style={styles.input}
+          placeholder="Nombre Completo"
+          value={fullname}
+          onChangeText={setFullname}
+        />
         <TextInput
           style={styles.input}
           placeholder="Email"
           keyboardType="email-address"
           autoCapitalize="none"
+          value={email}
+          onChangeText={setEmail}
         />
         <TextInput
           style={styles.input}
           placeholder="Contraseña"
           secureTextEntry={true}
+          value={password}
+          onChangeText={setPassword}
         />
-        <Pressable style={styles.button} onPress={() => {}}>
+        <Pressable style={styles.button} onPress={handleRegister}>
           <Text style={styles.buttonText}>Registrarse</Text>
         </Pressable>
         <Text style={styles.loginLink}>
@@ -46,15 +137,16 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "white",
+    flex: 1,
   },
   secondContainer: {
     marginTop: 8,
     justifyContent: "center",
     alignItems: "center",
-    height: "98%",
     width: "90%",
     backgroundColor: "#e5e7fd",
     borderRadius: 30,
+    paddingVertical: 20,
   },
   title: {
     fontSize: 40,
@@ -79,6 +171,7 @@ const styles = StyleSheet.create({
     paddingVertical: 12,
     paddingHorizontal: 25,
     borderRadius: 10,
+    marginTop: 10,
   },
   buttonText: {
     color: "black",
